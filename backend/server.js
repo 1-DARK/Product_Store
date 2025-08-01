d2c8a2ec4b0256f0c7d332d762b86ddf98622a00
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-01 22:54:11 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,38 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 dotenv.config();
 const app = express();
-app.get("/products", (req, res) => {
-  res.send("Server is Ready");
+app.use(express.json()); // allow us to accept the json data in the req.body
+app.post("/api/products", async (req, res) => {
+  const product = req.body; // user send this data
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({
+      success: false,
+      message: "Please provide all the fields",
+    });
+  }
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    return res.status(201).json({
+      success: true,
+      data: newProduct,
+    });
+  } catch (error) {
+    console.log("Error in Create Product", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
 });
-
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+  } catch (error) {}
+});
 app.listen(3000, () => {
   connectDB(); // db connected
   console.log("Server start at http://localhost:3000");
