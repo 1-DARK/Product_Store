8ec4579a5c1efbb9df8d18af18c0057d080a8c29
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-03 17:31:40 +0530

    Initial commit

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { CiSquarePlus } from "react-icons/ci";
+import { Container, Flex, HStack, Text } from "@chakra-ui/react";
+import { IconButton } from "@chakra-ui/react";
+import { useColorMode } from "@/components/ui/color-mode";
+import { LuMoon, LuSun } from "react-icons/lu";
+import { FiShoppingCart } from "react-icons/fi";
 
+import { Link } from "react-router-dom";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { toggleColorMode, colorMode } = useColorMode();
+  return (
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          fontSize={{ base: "22px", sm: "28px" }}
+          fontWeight="bold"
+          textTransform="uppercase"
+          textAlign="center"
+          color={"blue.500"}
+        >
+          <Link to={"/"}>
+            <HStack>
+              Product Store <FiShoppingCart />
+            </HStack>
+          </Link>
+        </Text>
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <IconButton variant="outline" size="lg">
+              <CiSquarePlus />
+            </IconButton>
+          </Link>
+          <IconButton onClick={toggleColorMode} variant="outline" size="lg">
+            {colorMode === "light" ? <LuSun /> : <LuMoon />}
+          </IconButton>
+        </HStack>
+      </Flex>
+    </Container>
+  );
 };
 
 export default Navbar;
--- frontend/src/pages/Createpage.jsx
+++ frontend/src/pages/Createpage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Createpage = () => {
-  return <div>create</div>;
+  return <div>Create</div>;
 };
 
 export default Createpage;
