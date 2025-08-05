d79f9d2cb8ef9b7fbbbfb7f917c425069c5fc205
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-05 22:11:27 +0530

    Initial commit

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,7 +1,26 @@
+import { Container, VStack, HStack, Text } from "@chakra-ui/react";
+import { IoRocketOutline } from "react-icons/io5";
+
 import React from "react";
 
 const Homepage = () => {
-  return <div>Home</div>;
+  return (
+    <Container maxW="2xl" py={"12"}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={{ base: "22px", sm: "28px" }}
+          fontWeight="bold"
+          textTransform="uppercase"
+          textAlign="center"
+          color={"blue.500"}
+        >
+          <HStack>
+            Current Products <IoRocketOutline />
+          </HStack>
+        </Text>
+      </VStack>
+    </Container>
+  );
 };
 
 export default Homepage;
