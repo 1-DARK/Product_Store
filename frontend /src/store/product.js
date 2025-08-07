147826fe1367010c7976e8d39081e6e6508d196d
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-07 23:08:33 +0530

    Initial commit

--- frontend /src/store/product.js
+++ frontend /src/store/product.js
@@ -34,7 +34,6 @@ export const useProductStore = create((set) => ({
     set((state) => ({
       products: state.products.filter((product) => product._id !== pid),
     }));
-
     return { success: true, message: data.message };
   },
   updateProduct: async (pid, formdata) => {
